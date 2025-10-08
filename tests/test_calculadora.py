d998303c167bcf39c410aec4233a189d2312abad
lab05l <lab05l@up-labs.local>
2025-10-07 22:10:01 -0300

    Ajuste calculadora e testes

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,4 +1,10 @@
-from src.calculadora import soma
+from src.calculadora import soma, subtracao, multiplicacao
 
 def test_soma():
   assert soma(2, 3) == 5
+
+def test_subtracao():
+  assert subtracao(5, 3) == 2
+
+def test_multiplicacao():
+  assert multiplicacao(2, 3) == 6
